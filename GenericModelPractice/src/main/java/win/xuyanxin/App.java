e4b98d4d865f15f2b331605f0d116129ad0e4f3f
XuYanXin <funnycodingxu@gmail.com>
2018-04-13 15:31:02 +0800

    Initial Maven Project And Add .gitignore file to ignore unrelated files

---
+++ GenericModelPractice/src/main/java/win/xuyanxin/App.java
@@ -0,0 +1,13 @@
+package win.xuyanxin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
